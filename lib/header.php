8cb460e84457efbfdca5506d3c96de20155e0800
Mrsh Ollo <69297097+mrshollo@users.noreply.github.com>
2023-01-23 05:41:35 +0700

    Update header.php

--- lib/header.php
+++ lib/header.php
@@ -3,10 +3,10 @@
 <head>
       <meta charset="UTF-8">
       <meta content="Tools Cek WR Mobile Legends" name="description" />
-      <meta content="PacificGroups" name="author" />
+      <meta content="MrshDev" name="author" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-      <meta name="keywords" content="cara menghitung wr, situs penghitung wr, mobile legends, hitung wr, website penghitung wr, penghitung wr, cara hitung wr ml">
+      <meta name="keywords" content="cek wr mlbb, cek winrate mobile legends, hitung wr mlbb, hitung winrate mobile legends, mobile legends, tools mobile legends, tools game,tools indonesia, cara menghitung wr, situs penghitung wr, mobile legends, hitung wr, website penghitung wr, penghitung wr, cara hitung wr ml">
       <meta property="og:description" content="Cek Win Rate Mobile Legends" />
       <title>Tools Cek WR Mobile Legends</title>
       
@@ -16,13 +16,8 @@
       <!-- General CSS Files -->
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-      
-      <!-- Adsense -->
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9610323528006222"
-     crossorigin="anonymous"></script>
-     
+           
       <!-- Template CSS -->
       <link rel="stylesheet" href="../assets/css/style.css">
-      <link rel="stylesheet" href="../assets/css/components.css">
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9610323528006222" crossorigin="anonymous"></script>
+      <link rel="stylesheet" href="../assets/css/components.css">      
 </head>
